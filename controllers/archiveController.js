3ed0abfb439a471657c6f25e000079be8f288b03
islam001sko <zhaslanuly01@gmail.com>
2024-02-12 23:34:15 +0600

    a

--- controllers/archiveController.js
+++ controllers/archiveController.js
@@ -1,22 +1,23 @@
 const axios = require('axios');
 
 exports.getArchive = async (req, res) => {
-    const year = req.query.year || "2021";
-    const month = req.query.month || "7";
-    const apiKey = process.env.NYT_API_KEY;
-
-    if (!year || !month) {
-        return res.render('archive', { articles: null, user: req.session.user });
-    }
-
-    const url = `https://api.nytimes.com/svc/archive/v1/${year}/${month}.json?api-key=${apiKey}`;
-
     try {
+        const type = req.query.filter || "emailed";
+
+        const days = req.query.time || "1";
+        const apiKey = process.env.NYT_API_KEY;
+
+        const url = `https://api.nytimes.com/svc/mostpopular/v2/${type}/${days}.json?api-key=${apiKey}`;
+
         const response = await axios.get(url);
         const data = response.data;
-        console.log(data);
-        const articles = data.response.docs.slice(0, 3);
-        res.render('archive', { articles, user: req.session.user });
+
+        if (data.status === "OK") {
+            const articles = data.results.slice(0, 3);
+            res.render('archive', { articles, user: req.session.user });
+        } else {
+            res.render('archive', { articles: null, user: req.session.user });
+        }
     } catch (error) {
         console.error('Error fetching archive data:', error);
         res.render('archive', { articles: null, user: req.session.user });
